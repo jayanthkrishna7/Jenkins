327ccbbfbac42c50db43399d95c6f96582b507cc
GRANDHI JAYANTH KRISHNA <88930227+jayanthkrishna7@users.noreply.github.com>
2023-01-18 08:25:22 +0530

    Update Main.java
    Sum program

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		int a,b;
+		Scanner sc= new Scanner(system.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int sum = a+b;
+		System.out.println(sum);
 	}
 }
