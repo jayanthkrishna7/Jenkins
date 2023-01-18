a8ae3ce19bd04d0179d1fff899cdf1de4ca966e3
GRANDHI JAYANTH KRISHNA <88930227+jayanthkrishna7@users.noreply.github.com>
2023-01-18 08:36:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ public class Main
 	public static void main(String[] args) {
 		int a,b;
 		Scanner sc= new Scanner(System.in);
-		a=sc.nextInt();
-		b=sc.nextInt();
+		a=10;
+		b=20;
 		int sum = a+b;
 		System.out.println(sum);
 	}
